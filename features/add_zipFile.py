baecaaa43438b77636d9319dae556d1cca13a24f
lovline <1079974085@qq.com>
2019-01-03 11:50:22 +0000

    Create add_zipFile.py

---
+++ features/add_zipFile.py
@@ -0,0 +1,7 @@
+I'm sorry to write here, because I don't know how to leave a message directly to you.
+I have been beset by a problem recently. I hope to get your answer.
+
+The question is：
+How Do I Insert a Compressed File into a Word File When the Python Is Processing a Word Document?
+
+Thanks very much.
